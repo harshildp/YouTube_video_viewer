540e26db3d3ac772bb72f080c0905a475ac59b21
Harshil Patel <h.gibbs62@gmail.com>
2018-03-09 13:15:45 -0600

    click to play and search throttle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import Video from './components/video';
 const API_KEY = 'AIzaSyBQThfvIYFoosihJ9ywYLYjsSQv7331zBQ'
 
 // Create a new component which produces some HTML
@@ -10,18 +12,33 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+        this.videoSearch('');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onTermChange={videoSearch} />
+                <Video video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
